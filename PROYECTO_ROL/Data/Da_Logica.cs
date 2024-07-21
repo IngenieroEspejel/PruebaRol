bea86cccdb085d61590a1dc430b5f7c763d26d18
Gus180474 <comercia.com@gmail.com>
2024-07-21 16:04:04 -0600

    Agregar archivos de proyecto.

---
+++ PROYECTO_ROL/Data/Da_Logica.cs
@@ -0,0 +1,19 @@
+﻿using PROYECTO_ROL.Models;
+
+namespace PROYECTO_ROL.Data
+{
+    public class Da_Logica
+    {
+        public List<Usuario> ListaUsuario() 
+        {
+            return new List<Usuario> 
+            {
+                new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
+                new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
+                new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
+                new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
+            }
+        }
+
+    }
+}
