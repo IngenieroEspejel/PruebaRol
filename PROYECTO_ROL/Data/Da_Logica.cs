c19fbb467f4c836f9cb2fd74f60997dee282685d
Gus180474 <comercia.com@gmail.com>
2024-07-21 18:08:30 -0600

    se corrige error en da_Logica.cs

--- PROYECTO_ROL/Data/Da_Logica.cs
+++ PROYECTO_ROL/Data/Da_Logica.cs
@@ -6,13 +6,13 @@ namespace PROYECTO_ROL.Data
     {
         public List<Usuario> ListaUsuario() 
         {
-            return new List<Usuario> 
+            return new List<Usuario>
             {
                 new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
                 new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
                 new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
                 new Usuario{Nombre="jose", Correo="administrado@gmail.com",Clave="123", Roles=new string[] {"Administrador"}},
-            }
+            };
         }
 
     }
